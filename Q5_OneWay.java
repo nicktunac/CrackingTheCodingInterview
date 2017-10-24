f970d0907df841bdc25c241d232a623286486dce
Nick Tunac <dominick.tunac@gmail.com>
2017-10-24 14:11:36 +0800

    Update solution for Q5

--- Q5_OneWay.java
+++ Q5_OneWay.java
@@ -20,40 +20,57 @@ import java.util.*;
 public class Q5_OneWay {
     public static void main(String args[]) {
 
-        System.out.println(oneAway("pale", "ple"));
-        System.out.println(oneAway("pales", "pale"));
-        System.out.println(oneAway("pale", "bale"));
-        System.out.println(oneAway("pale", "bake"));
-        System.out.println(oneAway("apple", "aple"));
+        System.out.println(oneEditAway("pale", "elp"));
+        System.out.println(oneEditAway("pales", "pales"));
+        System.out.println(oneEditAway("pale", "bale"));
+        System.out.println(oneEditAway("pale", "bake"));
+        System.out.println(oneEditAway("apple", "aple"));
     }
 
-    static boolean oneAway(String s, String t) {
+    static boolean oneEditAway(String first, String second) {
 
-        int sLength = s.length();
-        int tLength = t.length();
-        
-        if(Math.abs(sLength - tLength) > 1) 
-            return false;
-
-        Map<Character, Boolean> map = new HashMap<>();
-
-        for(int i = 0; i < sLength; i++) {
-            map.put(s.charAt(i), false);
+        if(first.length() == second.length()) {
+            return oneEditReplace(first, second);
+        } else if(first.length() + 1 == second.length()) {
+            return oneEditInsert(first, second);
+        } else if(second.length() + 1 == first.length()) {
+            return oneEditInsert(second, first);
         }
-
-        int count = 0;
-        for(int i = 0; i < tLength; i++) {
-            if(map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), true);
-                count++;
-            }
-        }
-
-        if(sLength - 1 == count) {
-            return true;
-        }
-
         return false;
     }
 
+    static boolean oneEditReplace(String s1, String s2) {
+        boolean foundDifference = false;
+
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                if (foundDifference) {
+                    return false;
+                }
+
+                foundDifference = true;
+            }
+        }
+        return true;
+    }
+
+    /* Check if you can insert a character into s1 to make s2. */
+    static boolean oneEditInsert(String s1, String s2) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while(index2 < s2.length() && index1 < s1.length()) {
+            if(s1.charAt(index1) != s2.charAt(index2)) {
+                if (index1 != index2) {
+                    return false;
+                }
+                index2++;
+
+            } else {
+                index1++;
+                index2++;
+            }
+        }
+        return true;
+    }
 }
